234d94ebbd8a4cc645fb0def7a2649089d5836a5
Sisco Falcó <siscofalcop@gmail.com>
2021-04-25 21:41:43 +0200

    Fixed preferences on user model, added foodtruck and owner registration routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -20,8 +20,23 @@ router.post('/client', (req, res) => {
     email,
     password,
     image,
-    vegan,
-    vegetarian,
+    food,
+    drinks,
+    bagels,
+    sandwiches,
+    burritos,
+    crepes,
+    hamburgers,
+    pizza,
+    sushi,
+    smoothies,
+    tea,
+    coffee,
+    beer,
+    cocktails,
+    iceCream,
+    cakes,
+    dessert
   } = req.body;
   Client.findOne({
       username
@@ -39,11 +54,26 @@ router.post('/client', (req, res) => {
             email,
             password: hashPass,
             image,
-            vegan: !!vegan,
-            vegetarian: !!vegetarian,
+            food: !!food,
+            drinks: !!drinks,
+            bagels: !!bagels,
+            sandwiches: !!sandwiches,
+            burritos: !!burritos,
+            crepes: !!crepes,
+            hamburgers: !!hamburgers,
+            pizza: !!pizza,
+            sushi: !!sushi,
+            smoothies: !!smoothies,
+            tea: !!tea,
+            coffee: !!coffee,
+            beer: !!beer,
+            cocktails: !!cocktails,
+            iceCream: !!iceCream,
+            cakes: !!cakes,
+            dessert: !!dessert
           })
           .then(() => {
-            res.redirect('/')
+            res.redirect('/private/profile')
           })
       }
 
@@ -53,6 +83,40 @@ router.post('/client', (req, res) => {
 
 router.get('/owner', (req, res) => {
   res.render('signup/owner')
+})
+
+router.post('owner', (req, res) => {
+  const {
+    username,
+    email,
+    password,
+    image,
+    NIF,
+    mobilephone
+  } = req.body;
+  Owner.findOne({
+    username
+  })
+  .then(owner => {
+    if (owner) {
+      res.render('signup/owner', {
+        errorMessage: 'User already exists'
+      })} else {
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashPass = bcrypt.hashSync(password, salt);
+        Owner.create({
+          username,
+          email,
+          password,
+          image,
+          NIF,
+          mobilephone
+        })
+        .then(() => {
+          res.redirect('/private/profile')
+        })
+      }
+    })
 })
 
 router.get('/client', (req, res) => {
@@ -68,27 +132,38 @@ router.get('/login', (req, res) => {
 });
 
 
-router.post('/login', (req, res) =>{
-  const { email, password } = req.body;
+router.post('/login', (req, res) => {
+  const {
+    email,
+    password
+  } = req.body;
 
-  if(!email || !password){
-    res.render('login', { errorMessage: 'Email and password are required'});
+  if (!email || !password) {
+    res.render('login', {
+      errorMessage: 'Email and password are required'
+    });
   }
 
-  Client.findOne({ email })
-  .then(user => {
-    if(!user){
-      res.render('login', { errorMessage: 'Incorrect email or password'});
-    }
+  Client.findOne({
+      email
+    })
+    .then(user => {
+      if (!user) {
+        res.render('login', {
+          errorMessage: 'Incorrect email or password'
+        });
+      }
 
-    const passwordCorrect = bcrypt.compareSync(password, user.password);
-    if(passwordCorrect){
-      req.session.currentUser = user;
-      res.redirect('/private/profile')
-    } else {
-      res.render('login', { errorMessage: 'Incorrect email or password'});
-    }
-  })
+      const passwordCorrect = bcrypt.compareSync(password, user.password);
+      if (passwordCorrect) {
+        req.session.currentUser = user;
+        res.redirect('/private/profile')
+      } else {
+        res.render('login', {
+          errorMessage: 'Incorrect email or password'
+        });
+      }
+    })
 })
 
 module.exports = router;
