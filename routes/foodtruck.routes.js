dd2738788fd51b247733ce1e441a1167ca183404
Susana Prado <susana.prado01@gmail.com>
2021-04-26 17:19:21 +0200

    search form

--- routes/foodtruck.routes.js
+++ routes/foodtruck.routes.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Foodtruck = require('../models/Foodtruck.model')
+const Foodtruck = require("../models/Foodtruck.model");
 
-router.get('/register', (req, res) => {
-  res.render('foodtruck/register')
+router.get("/register", (req, res) => {
+  res.render("foodtruck/register");
 });
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   const {
     name,
     description,
@@ -30,47 +30,63 @@ router.post('/register', (req, res) => {
     cocktails,
     iceCream,
     cakes,
-    dessert
+    dessert,
+    any
   } = req.body;
   Foodtruck.findOne({
-      name
-    })
-    .then(foodtruck => {
-      if (foodtruck) {
-        res.render('foodtruck/register', {
-          errorMessage: 'Foodtruck already registered'
-        })
-      } else {
-        Foodtruck.create({
-            name,
-            description,
-            images,
-            price,
-            date,
-            availability,
-            food: !!food,
-            drinks: !!drinks,
-            bagels: !!bagels,
-            sandwiches: !!sandwiches,
-            burritos: !!burritos,
-            crepes: !!crepes,
-            hamburgers: !!hamburgers,
-            pizza: !!pizza,
-            sushi: !!sushi,
-            smoothies: !!smoothies,
-            tea: !!tea,
-            coffee: !!coffee,
-            beer: !!beer,
-            cocktails: !!cocktails,
-            iceCream: !!iceCream,
-            cakes: !!cakes,
-            dessert: !!dessert
-          })
-          .then(() => {
-            res.redirect('/')
-          })
-      }
-    })
-})
+    name,
+  }).then((foodtruck) => {
+    if (foodtruck) {
+      res.render("foodtruck/register", {
+        errorMessage: "Foodtruck already registered",
+      });
+    } else {
+      Foodtruck.create({
+        name,
+        description,
+        images,
+        price,
+        date,
+        availability,
+        food: !!food,
+        drinks: !!drinks,
+        bagels: !!bagels,
+        sandwiches: !!sandwiches,
+        burritos: !!burritos,
+        crepes: !!crepes,
+        hamburgers: !!hamburgers,
+        pizza: !!pizza,
+        sushi: !!sushi,
+        smoothies: !!smoothies,
+        tea: !!tea,
+        coffee: !!coffee,
+        beer: !!beer,
+        cocktails: !!cocktails,
+        iceCream: !!iceCream,
+        cakes: !!cakes,
+        dessert: !!dessert,
+        any: !!any,
+      }).then(() => {
+        res.redirect("/");
+      });
+    }
+  });
+});
+
+router.get("/results", (req, res) => {
+  res.render("foodtruck/foodtruck-list");
+});
+
+router.post('/results', (req, res) =>{
+  const {type, specialty, price, date} = req.body;
+  Foodtruck.find({
+
+  })
+  .then((results) =>{
+    console.log(results);
+    res.render("foodtruck/foodtruck-list", {foodtrucks: results});
+  })
+  .catch(error => console.error(error))
+});
 
 module.exports = router;
