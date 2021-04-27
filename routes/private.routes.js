64152d7718f82f2ef99e981aa3e54dd2367c1d81
Sisco Falcó <siscofalcop@gmail.com>
2021-04-27 18:58:01 +0200

    fixed foodtruck by owner

--- routes/private.routes.js
+++ routes/private.routes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 // const Owner = require('../models/Owner.model');
 const router = express.Router();
+const Client = require('../models/Client.model');
+const Owner = require('../models/Owner.model');
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
 
 function isLoggedIn(req, res, next) {
   if (req.session.currentUser) next();
@@ -8,17 +12,80 @@ function isLoggedIn(req, res, next) {
 }
 
 router.get('/profile', isLoggedIn, (req, res) => {
-  res.render('profile', { client: req.session.currentUser });
+  res.render('client/profile', { client: req.session.currentUser });
 });
 
 router.get('/profile-owner', isLoggedIn, (req, res) => {
-  // Owner.findById({_id: req.currentsession.currentUser._id})
-  // // .populate('foodtrucks')
-  // .then(()=> {
-  //   // foundOwner
-  //   console.log(foundOwner)
-    res.render('profile-owner', { owner: req.session.currentUser })
-  // })
+  Owner.findById({_id: req.session.currentUser._id}).populate('foodtrucks').then((user) => {
+    console.log(user)
+    res.render('profile-owner', { owner: user })
+  })
+});
+
+router.get('/profile/edit', (req, res) => {
+  res.render('client/client-update-form', { client: req.session.currentUser });
+});
+
+router.post('/profile/edit', (req, res) => {
+  const id = req.session.currentUser._id;
+  const { username, email, password, image } = req.body;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hashPassword = bcrypt.hashSync(password, salt);
+
+  Client.findByIdAndUpdate(id, {
+    username,
+    email,
+    password: hashPassword,
+    image,
+  })
+    .then(() => {
+      res.redirect('/private/profile');
+    })
+    .catch((error) => console.error(error));
+});
+
+router.get('/profile-owner/edit', (req, res) => {
+  res.render('owner/owner-update-form', { owner: req.session.currentUser });
+});
+
+router.post('/profile-owner/edit', (req, res) => {
+  const id = req.session.currentUser._id;
+  const { username, email, password, image, NIF, mobilephone } = req.body;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hashPss = bcrypt.hashSync(password, salt);
+
+  Owner.findByIdAndUpdate(id, {
+    username,
+    email,
+    password: hashPss,
+    image,
+    NIF,
+    mobilephone,
+  })
+    .then(() => {
+      res.redirect('/private/profile-owner');
+    })
+    .catch((error) => console.error(error));
+});
+
+router.post('/profile', (req, res) => {
+  const id = req.session.currentUser._id;
+
+  Client.findByIdAndDelete(id)
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch((error) => console.error(error));
+});
+
+router.post('/profile-owner', (req, res) => {
+  const id = req.session.currentUser._id;
+
+  Owner.findByIdAndDelete(id)
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch((error) => console.error(error));
 });
 
 module.exports = router;
