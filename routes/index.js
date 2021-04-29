6de8b9209119362946be4f476dc6f8a9e32c25df
Susana Prado <susana.prado01@gmail.com>
2021-04-29 18:03:11 +0200

    css fixing

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res) => {
-  if(req.session.currentUser && req.session.currentUser._id){
-    res.render('index', {layout: "layout-user.hbs"});
-  } else {
-    res.render('index');
-  }
+    res.render('index', { user: req.user, layout: "layout-user.hbs"});
 });
 
 // router.get('/foodtruck-list', (req, res) => {
